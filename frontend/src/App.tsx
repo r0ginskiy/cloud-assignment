b362383ac21c81027806a06e401f32c1067b3249
AlexeiRoginskii <roginskiial@gmail.com>
2025-09-15 21:21:50 +0300

    init: frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box textAlign="center" p={10}>
+      <Heading>Cloud Assignment Frontend</Heading>
+      <Button colorScheme="teal" mt={5}>
+        Test Button
+      </Button>
+    </Box>
+  );
+}
+
+export default App;
